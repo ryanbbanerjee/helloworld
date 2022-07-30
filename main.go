a75dc2ba11fdf451c8675849547bebaac1305440
Ryan Banerjee <ryanbbanerjee@gmail.com>
2022-07-30 12:47:00 -0700

    my first go program

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("hello world")
+	fmt.Println("hello world")
+	one := 1
+	two := 2
+	fmt.Println(one)
+	fmt.Println(two)
+	three := one + two
+	fmt.Println(three)
 }
